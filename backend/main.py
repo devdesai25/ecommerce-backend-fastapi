b5a2d1d16e0a68603178d35412d500d66ab0c8b6
devdesai25 <desaidev1610@gmail.com>
2026-03-03 00:40:54 +0530

    Added CORSMiddleware to accept data from frontend

--- backend/main.py
+++ backend/main.py
@@ -2,10 +2,18 @@ from fastapi import FastAPI
 from config import settings
 from database import engine, Base, metadata
 from routes import login_router, get_products_router, signup_router, me_router, admin_router, create_product_router, delete_product_router, update_product_router, route
-
+from fastapi.middleware.cors import CORSMiddleware
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = ["http://localhost:5173"],
+    allow_credentials = True,
+    allow_headers = ["*"],
+    allow_methods = ["*"]
+)
 
 app.include_router(login_router)
 app.include_router(get_products_router)
