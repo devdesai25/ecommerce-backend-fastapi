f0eac46b0e79cbce5cc15c579c15b83671a7f99b
devdesai25 <desaidev1610@gmail.com>
2026-03-03 01:09:11 +0530

    Added Signup Page

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,5 +1,45 @@
+import { useState } from "react";
+
 function Signup(){
-    return (<h1>This is Signup</h1>)
+   
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = async() => {
+
+        const res = await fetch("http://localhost:8000/signup",{
+            method : "POST",
+            headers : {
+                "Content-Type" : "application/json",
+            },
+            body : JSON.stringify({
+                username : username,
+                password : password
+            })
+
+        });
+
+        const data = await res.json()
+
+        console.log(data)
+    }
+    
+    return (<div>
+        <input 
+        placeholder="Username"
+        value = {username}
+        onChange = {(e)=>setUsername(e.target.value)}
+        />
+
+        <input 
+        placeholder="Password"
+        value = {password}
+        onChange = {(e)=>setPassword(e.target.value)}
+        />
+
+        <button onClick={ handleSubmit}>Submit</button>
+
+    </div>)
 }
 
 export default Signup;
