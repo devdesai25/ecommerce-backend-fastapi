6597bfd1f6bbf0eae0e9969860c68adb5f1f715f
devdesai25 <desaidev1610@gmail.com>
2026-03-03 22:10:37 +0530

    Added Home page Product view

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,33 @@
+import { useState, useEffect } from "react"
+
 function Home(){
-    return (<h1>This is home page</h1>)
+    
+    const [products, setProduct] = useState([])
+
+    useEffect(()=>{
+        const fetchProducts = async () => {
+        try{    const res = await fetch("http://localhost:8000/products");
+            const data = await res.json();
+            setProduct(data);
+        }catch(error){
+            console.log("Error fetching products",error)
+        }
+    };
+    fetchProducts()
+
+}, []);
+    
+    return (<div>
+        {products.map( product =>(
+            <div>
+                <h3>{product.name}</h3>
+                <h3>{product.price}</h3>
+                <h3>{product.description}</h3>
+                <br></br>
+                <br></br>
+            </div>
+        ))}
+    </div>)
 }
+
 export default Home
