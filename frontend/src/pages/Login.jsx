1d2d480936ad0af9e3457d8254f664ea84a21843
devdesai25 <desaidev1610@gmail.com>
2026-03-11 22:34:15 +0530

    Refactored to use API Layer

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,7 @@
 import { useContext, useState } from "react";
 import { useNavigate} from 'react-router-dom'
 import { AuthContext } from "../context/AuthContext";
+import  api  from "../services/api";
 
 function Login(){
 
@@ -14,19 +15,14 @@ function Login(){
         formData.append("username", username);
         formData.append("password", password);
 
-        const res = await fetch("http://localhost:8000/login",{
-            method : "POST",
-            headers : {
-                "Content-Type" : "application/x-www-form-urlencoded",
-            },
-            body : formData
-        });
+        const res = api.post("/login",formData,{headers:{
+            "Content-Type": "application/x-www-form-urlencoded"
+        }})
 
-        const data = await res.json()
-        console.log(data)
+        console.log(res)
 
-        localStorage.setItem("token", data.access_token);
-        localStorage.setItem("role",JSON.stringify(data.user));
+        localStorage.setItem("token", res.access_token);
+        localStorage.setItem("role",JSON.stringify(res.user));
         setIsLoggedIn(true);
         navigate("/");
 
