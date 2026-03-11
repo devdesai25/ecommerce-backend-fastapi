f42b0c8624defac5fd9e8ffa9cbb5c8383bb5990
devdesai25 <desaidev1610@gmail.com>
2026-03-11 18:44:48 +0530

    Refactored

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -1,6 +1,7 @@
 import { useContext } from "react"
 import { AuthContext } from "../context/AuthContext"
 import { Navigate } from "react-router-dom";
+
 function ProtectedRoute({ children }) {
 
     const {isLoggedIn} = useContext(AuthContext);
