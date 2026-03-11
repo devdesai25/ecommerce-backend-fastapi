4b9f47e0d4ede6f535fabb60aa4c416bcf971efe
devdesai25 <desaidev1610@gmail.com>
2026-03-11 18:53:24 +0530

    Added API Layer

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -7,7 +7,7 @@ const api = axios.create({
     }
 })
 
-api.interceptors.use((config) => {
+api.interceptors.request.use((config) => {
     const token = localStorage.getItem("token")
 
     if (token) {
