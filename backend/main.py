5d0b45e11fcb435f747cda78e6456a48f8524381
devdesai25 <desaidev1610@gmail.com>
2026-04-21 21:29:06 +0530

    Added vercel.json

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,7 @@ from config import settings
 from database import engine, Base, metadata
 from routes import login_router, get_products_router, signup_router, me_router, admin_router, create_product_router, delete_product_router, update_product_router, route
 from fastapi.middleware.cors import CORSMiddleware
+from mangum import Mangum
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
@@ -24,3 +25,5 @@ app.include_router(create_product_router)
 app.include_router(delete_product_router)
 app.include_router(update_product_router)
 app.include_router(route)
+
+handler = Mangum(app)
