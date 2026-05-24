3324851d36ed0a9f254b792d8e9516d6dee5fb41
devdesai25 <desaidev1610@gmail.com>
2026-05-24 22:00:15 +0530

    Added Cart DataModel

---
+++ backend/models/cart_items.py
@@ -0,0 +1,25 @@
+from database import  Base
+from sqlalchemy.sql import func
+from sqlalchemy import String, Integer, ForeignKey,DateTime, Column, UniqueConstraint
+
+class Cart(Base):
+    
+    __tablename__ = "cart_items"
+
+    id = Column(Integer, primary_key=True, index=True, nullable=False)
+
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False, )
+    product_id = Column(Integer, ForeignKey("products.product_id"), nullable=False)
+
+    quantity = Column(Integer, nullable=False)
+
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
+
+    __table__args__ = (
+        UniqueConstraint(
+            "user_id",
+            "product_id",
+            name="unique_user_product"
+        )
+    )
