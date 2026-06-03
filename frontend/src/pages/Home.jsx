f4066b1428b1e3412b4469927a4130ef070b0005
devdesai25 <desaidev1610@gmail.com>
2026-06-03 18:44:57 +0530

    Added images and add to cart button for products

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -21,16 +21,32 @@ function Home(){
 
     }, []);
     
+    const addToCart = async (productId) => {
+        try{
+            const res = await api.post("cart", 
+                {
+                    product_id: productId,
+                    quantity: 1
+                })
+            
+            console.log(res)
+        }catch(error){
+            console.log("Error Fetching Products", error)
+        }
+    }
+
     return (<div className="product-grid">
         {products.map(product => (
             <div key={product.product_id} className="card">
-                    <h3>{product.name}</h3>
+                <img src={product.images} alt={product.name} />    
+                <h3>{product.name}</h3>
                 <p className="price">
                     {product.price}
                 </p>
                 <p className="description">
                     {product.description}
                 </p>
+                <button onClick={() => addToCart(product.product_id)}>Add To Cart</button>
             </div>
         ))}
     </div>)
