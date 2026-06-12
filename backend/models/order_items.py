2e7c35a481a836896c727e0af9bf6ecabb29e1fb
devdesai25 <desaidev1610@gmail.com>
2026-06-12 21:10:48 +0530

    Created order_items data model to keep track of items ordered

---
+++ backend/models/order_items.py
@@ -0,0 +1,34 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, Float
+from sqlalchemy.orm import relationship
+from database import Base
+
+class OrderItem(Base):
+    __tablename__ = "order_items"
+
+    order_item_id = Column(Integer, primary_key=True, index=True)
+    
+    product_id = Column(
+        Integer, 
+        ForeignKey("products.product_id"), 
+        index=True
+    )
+    order_id = Column(
+        Integer, 
+        ForeignKey("orders.order_id"),
+        index=True
+        )
+    quantity = Column(Integer, default=1)
+
+    unit_price = Column(Float, nullable=False)
+
+    subtotal = Column(Float)
+
+    order = relationship(
+        "Order",
+        back_populates="order_items"
+    )
+
+    product = relationship(
+        "Product",
+        back_populates="order_items"
+    )
