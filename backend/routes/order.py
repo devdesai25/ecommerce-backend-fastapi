47e3076356db6cc8ec45af9131e407ad61373bf2
devdesai25 <desaidev1610@gmail.com>
2026-06-13 18:19:18 +0530

    Consolidated all order routes

---
+++ backend/routes/order.py
@@ -0,0 +1,42 @@
+from fastapi import APIRouter, Depends
+from database import get_db
+from services.auth import get_current_user
+from services.order_service import orderCreate, orderGet, orderPay, orderCancel
+from sqlalchemy.orm import Session
+from schemas.orders import OrderRequest
+from models.users import User
+
+router = APIRouter(tags=["Order"])
+
+@router.get("/orders")
+def get_order(
+    user: User=Depends(get_current_user),
+    db:Session=Depends(get_db)
+    ):
+    
+    return orderGet(user, db)
+
+@router.post("/checkout")
+def create_order(
+    address: OrderRequest,
+    user: User = Depends(get_current_user),
+    db: Session = Depends(get_db)
+):
+    return orderCreate(address, user, db)
+
+@router.patch("/orders/{id}/pay")
+def pay_order(
+    id: int,
+    user: User = Depends(get_current_user),
+    db: Session = Depends(get_db)
+):
+
+    return orderPay(id, user, db)
+
+@router.patch("/oders/{id}/cancel")
+def cancel_order(
+    id:int,
+    user:User=Depends(get_current_user),
+    db:Session=Depends(get_db)
+):
+    return orderCancel(id, user, db)
