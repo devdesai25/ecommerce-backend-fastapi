0490809f9ffbc000018421b3b0ea8e35380e0e16
devdesai25 <desaidev1610@gmail.com>
2026-06-13 18:19:40 +0530

    Consolidated all product routes

---
+++ backend/routes/products.py
@@ -0,0 +1,56 @@
+from fastapi import APIRouter, Depends
+from services.product_service import productadd, productDelete, productUpdate
+from services.auth import req_admin
+from schemas.product import ProductCreate, ProductResponse, ProductUpdate
+from sqlalchemy.orm import Session
+from models.users import User
+from models.products import Product
+from database import get_db
+
+router = APIRouter(
+    prefix="/admin",
+    tags=["Create"]
+)
+
+@router.get("/")
+def home():
+    return {"message":"backend connected"}
+
+@router.get("/products", response_model= list[ProductResponse])
+def get_products(
+    limit : int = 10, 
+    offset : int = 0,
+    db: Session = Depends(get_db )
+):
+    
+    all_prod = db.query(Product).offset(offset).limit(limit).all()
+    
+    return all_prod
+
+@router.post("/create")
+def create_product(
+    cur_product : ProductCreate,
+    admin : User = Depends(req_admin), 
+    db : Session = Depends(get_db)
+):
+    
+    return productadd(cur_product, admin, db)
+
+@router.delete("/delete/{id}")
+def delete_prod(
+    id : int,
+    admin : User = Depends(req_admin),
+    db : Session =  Depends(get_db)
+):
+
+    return productDelete(id, db)
+
+@router.patch("/update/{id}")
+def update_product(
+    id : int ,
+    cur_update : ProductUpdate,
+    admin : User = Depends(req_admin), 
+    db : Session = Depends(get_db)
+):
+
+    return productUpdate(id, cur_update, db)
